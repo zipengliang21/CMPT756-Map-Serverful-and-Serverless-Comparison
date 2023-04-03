3adc2d8d7aff25bd1b2d38fda0073b0a11190a6d
Zipeng Liang <zipengliang21@gmail.com>
2023-04-03 16:38:52 -0700

    Include navigation_db_user argument

--- src/navigation/main_initialize_map.py
+++ src/navigation/main_initialize_map.py
@@ -5,12 +5,14 @@ from src.navigation.database.connection import CreateDatabaseConnection
 from src.navigation.mutation.writer import MutationWriter
 
 
-def _InitializeMap(navigation_db_host: str, navigation_db_password: str):
+def _InitializeMap(
+        navigation_db_host: str, navigation_db_user: str,
+        navigation_db_password: str):
     mutations = GenerateInitialMap()
 
     db_conn = CreateDatabaseConnection(
         navigation_db_host=navigation_db_host,
-        navigation_db_user="zipeng",
+        navigation_db_user=navigation_db_user,
         navigation_db_password=navigation_db_password)
 
     writer = MutationWriter(db_conn=db_conn)
@@ -25,6 +27,9 @@ if __name__ == "__main__":
         "--navigation_db_host", type=str,
         help="The IP address which points to the navigation database server.")
     parser.add_argument(
+        "--navigation_db_user", type=str,
+        help="The username of the navigation postgres database.")
+    parser.add_argument(
         "--navigation_db_password",
         type=str,
         help="The password of the navigation postgres database user.")
@@ -34,9 +39,13 @@ if __name__ == "__main__":
     if args.navigation_db_host is None:
         print("navigation_db_host is missing.")
         exit(-1)
+    if args.navigation_db_user is None:
+        print("navigation_db_user is missing.")
+        exit(-1)
     if args.navigation_db_password is None:
         print("navigation_db_password is missing.")
         exit(-1)
 
     _InitializeMap(navigation_db_host=args.navigation_db_host,
+                   navigation_db_user=args.navigation_db_user,
                    navigation_db_password=args.navigation_db_password)
